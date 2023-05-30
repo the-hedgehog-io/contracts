ca1f93ce8bd85244f059a244467b79a46ad0e377
georgyskr <gosha.skryuchenkov@gmail.com>
2023-05-30 19:57:39 +0300

    feat: initial commit

---
+++ contracts/BaseFeeOracle.sol
@@ -0,0 +1,36 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract BaseFeeOracle is AccessControl {
+    struct Response {
+        int256 answer;
+        uint256 timestamp;
+        uint80 roundId;
+    }
+
+
+    mapping(uint256 => Response) public responseById;
+
+    bytes32 internal constant SETTER = keccak256("SETTER");
+    bytes32 internal constant ULTIMATE_ADMIN = keccak256("ULTIMATE_ADMIN");
+
+    event BaseFeeSet(int256 newValue);
+
+    constructor(address _admin, address _ultimateAdmin) {
+        _grantRole(ULTIMATE_ADMIN, _ultimateAdmin);
+        _setRoleAdmin(SETTER, ULTIMATE_ADMIN);
+        _grantRole(SETTER, _admin);
+    }
+
+    function setBaseFee(int256 _newValue) external onlyRole(SETTER) {
+        latestRoundData = Response({
+            answer: _newValue,
+            timestamp: block.timestamp
+            roundId: latestRoundData
+        });
+
+        emit BaseFeeSet(_newValue);
+    }
+}
