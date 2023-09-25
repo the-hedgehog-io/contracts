62a0d02f42f831df67d48f934e1a267c8f6db86e
georgyskr <gosha.skryuchenkov@gmail.com>
2023-09-25 20:07:57 +0300

    feat: update tests variables

--- test/hedgehog-tests/smoke/config/abc.ts
+++ test/hedgehog-tests/smoke/config/abc.ts
@@ -10,7 +10,9 @@ const ABCConfig = () => {
   const AliceDecreaseDebtFirst = "2000000";
   const AliceDebtAfterFirstDecrease = "5000000";
   const AliceCRAfterFirstDecrease = 233;
-  // TODO: Stopped on H21
+  const AliceTroveCollAfterBobRedemption = ethers.parseEther("0.332"); //TODO: Probably to exact enough
+  const AliceTroveDebtAfterBobRedemption = 4440000;
+  const AliceCRAfterBobRedemption = 250;
   const AliceRedemptionFirst = "4915000";
 
   const BobTroveColl = ethers.parseEther("2");
@@ -18,11 +20,19 @@ const ABCConfig = () => {
   const BobInitialCR = 1333;
   const BobUnstakeFirst = "560000";
   const BobRedemptionFirst = "560000";
-  const BobTroveIncreaseCollFirst = ethers.parseEther("0.00167");
+  const BobTroveIncreaseCollFirst = "16663244859813100";
+  const BobTroveCollAfterIncrease = "2016663244859810000";
+  const BobCRAfterIncrease = 672;
+  const BobTroveCollAfterLuquid = "2059754172045110000";
+  const BobTroveDebtAfterLuquid = "5805881";
+  const BobCRAfterLiquid = 591;
+  // TODO: Stopped on I37. Update Carol's variables
+
   const BobTroveIncreaseDebtSecond = "3000000";
 
   const CarolTroveColl = ethers.parseEther("3");
   const CarolTroveDebt = "4000000";
+  const CarolInitialCR = 2500;
   const CarolIncreaseDebt = "400000";
   const CarolRepayment = "100000";
 };
